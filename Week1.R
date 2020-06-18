d1acf0e15c2aee9beb0bc5ea79bea7e232bdf44a
ynren1020 <renxx275@umn.edu>
2020-06-18 10:36:53 -0500

    First week learning notes

---
+++ Week1.R
@@ -0,0 +1,12 @@
+# Notes ---
+getwd()
+setwd()
+#relative or absolute path
+
+file.exists() # check if a directory exisits
+dir.create() # create a directory
+
+if (!file.exists("data")){
+    dir.create("data")
+}
+
